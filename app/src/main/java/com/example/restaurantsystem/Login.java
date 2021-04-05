a18478f628a2f2a28ed37da51b1671f0a71fad9d
ceydaelmas <20180808037@ogr.akdeniz.edu.tr>
2021-04-05 10:59:19 +0300

    Restaurant System initial commit

--- app/src/main/java/com/example/restaurantsystem/Login.java
+++ app/src/main/java/com/example/restaurantsystem/Login.java
@@ -21,6 +21,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+
 public class Login extends AppCompatActivity {
     private FirebaseAuth mAuth;
     TextView forgotTextLink;
